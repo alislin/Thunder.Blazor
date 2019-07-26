a030605694dde9bb5f025b23310bba1f5e885eaf
Alis Lin <Alis.Lin@outlook.com>
2019-07-26 21:59:08 +0800

    update

--- Thunder.Blazor/Thunder.Blazor/Services/ChangedEventArgs.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Thunder.Blazor.Services
-{
-    public class ChangedEventArgs
-    {
-        public string Key { get; set; }
-        public object OldValue { get; set; }
-        public object NewValue { get; set; }
-    }
-}
--- Thunder.Blazor/Thunder.Blazor/Services/ChangingEventArgs.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Thunder.Blazor.Services
-{
-    public class ChangingEventArgs : ChangedEventArgs
-    {
-        public bool Cancel { get; set; }
-    }
-}
