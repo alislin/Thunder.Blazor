2364a6362ae35ff1abf44676b3b34bdf829c8f9f
Alis Lin <5921000@qq.com>
2019-08-20 14:09:59 +0800

    fix iisexpress run,fix dropdownitem active style

--- Thunder.Blazor/Thunder.Blazor/Components/TNodeComponent.cs
+++ Thunder.Blazor/Thunder.Blazor/Components/TNodeComponent.cs
@@ -1,16 +1,12 @@
 ﻿/* Ceated by Ya Lin. 2019/7/10 10:42:48 */
 
-using System;
 using Microsoft.AspNetCore.Components;
+using System;
 using Thunder.Blazor.Services;
 
 namespace Thunder.Blazor.Components
 {
-    /// <summary>
-    /// 内容区域基础类
-    /// </summary>
-    /// <typeparam name="TModel"></typeparam>
-    public class TBlockComponent<TModel> : TComponent<TModel> where TModel: TNode<TModel>, new()
+    public class TNodeComponent<TModel> : TComponentObject<Node<TModel>> where TModel : TagBlockContext,new()
     {
         [Inject] public ComponentService ComponentService { get; set; }
         /// <summary>
@@ -22,30 +18,70 @@ namespace Thunder.Blazor.Components
         /// </summary>
         [Parameter] public bool IsDisabledAutoClose { get; set; }
 
-        public override void UpdateDataContext()
-        {
-            if (DataContext != null)
-            {
-                DataContext.IsOpen = IsOpen;
-            }
-            base.UpdateDataContext();
-        }
-
         public override void LoadDataContext()
         {
             if (DataContext != null)
             {
                 IsOpen = DataContext.IsOpen;
             }
-            base.LoadDataContext();
+            LoadDataContext(dataContext.Context);
         }
 
+        public override void UpdateDataContext()
+        {
+            if (DataContext != null)
+            {
+                DataContext.IsOpen = IsOpen;
+            }
+            UpdateDataContext(dataContext.Context);
+        }
         protected override void OnInitialized()
         {
             base.OnInitialized();
-            DataContext.Close = Close;
+            if (HasParamenters)
+            {
+                try
+                {
+                    dataContext.Context = Paramenters.Get<TModel>();
+                }
+                catch
+                {
+                }
+            }
+            if (dataContext != null)
+            {
+                dataContext.Context.SetViewAction(this);
+                //DataContext.StateHasChanged = StateHasChanged;
+            }
+            dataContext.Close = Close;
         }
 
+        /// <summary>
+        /// 加载
+        /// </summary>
+        public override void Load()
+        {
+            Show();
+        }
+        /// <summary>
+        /// 显示 / 激活
+        /// </summary>
+        public override void Show()
+        {
+            LoadDataContext();
+            base.Show();
+            UpdateDataContext();
+            //dataContext.IsVisabled = base.IsVisabled;
+        }
+
+        /// <summary>
+        /// 设置子组件
+        /// </summary>
+        /// <param name="child">子组件数据</param>
+        public override void SetChild(TContext child)
+        {
+            SetChild(child, dataContext.Context);
+        }
         public void ToggleShow()
         {
             IsVisabled = !IsVisabled;
@@ -119,6 +155,5 @@ namespace Thunder.Blazor.Components
             ComponentService.DoAction("openblock");
             commandAction?.Invoke();
         }
-
     }
 }
