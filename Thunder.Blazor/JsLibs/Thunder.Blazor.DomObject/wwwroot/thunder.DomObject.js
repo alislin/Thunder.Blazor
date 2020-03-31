27d22fa0e622f1c185d8178a8ee7314579cd70b6
林亚 <Alis.Lin@outlook.com>
2020-03-31 09:54:15 +0800

    update to last

---
+++ Thunder.Blazor/JsLibs/Thunder.Blazor.DomObject/wwwroot/thunder.DomObject.js
@@ -0,0 +1,44 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var Thunder;
+(function (Thunder) {
+    var DomObject;
+    (function (DomObject) {
+        var ThunderBlazor = /** @class */ (function () {
+            function ThunderBlazor() {
+            }
+            return ThunderBlazor;
+        }());
+        var BlazorCallback = /** @class */ (function () {
+            function BlazorCallback() {
+                this.Callback = "CallAction";
+                this.Confirm = "ConfirmCallback";
+                this.Cancel = "CancelCallback";
+            }
+            return BlazorCallback;
+        }());
+        function Init() {
+            var obj = {
+                Callback: new BlazorCallback()
+            };
+            if (window.ThunderBlazor) {
+                window.ThunderBlazor = __assign(__assign({}, window.ThunderBlazor), obj);
+            }
+            else {
+                window.ThunderBlazor = __assign({}, obj);
+            }
+        }
+        DomObject.Init = Init;
+    })(DomObject = Thunder.DomObject || (Thunder.DomObject = {}));
+})(Thunder || (Thunder = {}));
+Thunder.DomObject.Init();
+//# sourceMappingURL=thunder.DomObject.js.map
